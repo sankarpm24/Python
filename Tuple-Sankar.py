62ddcd49e2246f6f9e8ffa1b4129f53055684cdc
sankarpm24 <53351504+sankarpm24@users.noreply.github.com>
2019-08-18 16:13:09 +0530

    Add files via upload

---
+++ Tuple-Sankar.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+mytuple=["vijay","Ajith","Suriya","Sedhupathi"]
+for x in mytuple:
+    if x=="vijay":
+     print("My fav")
+    else:
+        print("Tamil hero")
+        
+
+
+# In[2]:
+
+
+mytuple=["vijay","Ajith","Suriya","Sedhupathi"]
+"Ravi"in mytuple
+
+
+# In[4]:
+
+
+mytuple=["vijay","Ajith","Suriya","Sedhupathi"]
+"Suriya"in mytuple
+
+
+# In[5]:
+
+
+a=input("Enter your name :")
+print(a)
+
+
+# In[6]:
+
+
+myscores=['50','80','87','66','100']
+Checklist=input("Enter your sore to check eligibility")
+if Checklist >='79':
+ print("Awesome, PASS")
+else:
+ print("Fail")
+
+    
+
+
+# In[7]:
+
+
+myscores=['50','80','87','66','100']
+Checklist=input("Enter your sore to check eligibility")
+if Checklist >='79':
+ print("Awesome, PASS")
+else:
+ print("Fail")
+
+
+# In[10]:
+
+
+Age=['50','15','29','25','22']
+myage=input("Enter your age to check eligibility")
+if myage in Age:
+ if myage >='18':
+  print("Eligible for voting")
+ else:
+  print("Minor")
+else:
+ print("Not eligible- not in list")
+
+
+# In[11]:
+
+
+Age=['50','15','29','25','22']
+myage=input("Enter your age to check eligibility")
+if myage in Age:
+ if myage >='18':
+  print("Eligible for voting")
+ else:
+  print("Minor")
+else:
+ print("Not eligible- not in list")
+
+
+# In[ ]:
+
+
+
+
